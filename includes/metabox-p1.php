8e86db240dd425779738a2aaff9b47b2c75a34bf
jorge.ochoa@nivelics.co-at-982536497151 <jorge.ochoa@nivelics.co-at-982536497151>
2021-02-04 20:36:01 -0500

    ajuste borrar

--- includes/metabox-p1.php
+++ includes/metabox-p1.php
@@ -8,7 +8,7 @@ function wpbc_contacts_page_handler()
 
     $message = '';
     if ('delete' === $table->current_action()) {
-        $message = '<div class="updated below-h2" id="message"><p>' . sprintf(__('Items deleted: %d', 'wpbc'), count($_REQUEST['id'])) . '</p></div>';
+        $message = '<div class="updated below-h2" id="message"><p>' . sprintf(__('se borró el banner: %d', 'wpbc'), $_REQUEST['id']) . '</p></div>';
     }
     ?>
 <div class="wrap">
